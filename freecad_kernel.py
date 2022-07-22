72104f498962e354bce2891868b9e7b31a509bf0
Alexandre Milani <alexandre.milani00@gmail.com>
2022-07-21 20:56:22 -0400

    Reversed part of the last commit.

--- freecad_kernel.py
+++ freecad_kernel.py
@@ -45,3 +45,8 @@ class FreeCADKernel (Kernel):
             'payload': [],
             'user_expressions': {},
         }
+
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=FreeCADKernel)
